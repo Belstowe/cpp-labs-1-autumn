d19d192aed29b069346009d1d97476f45fa1a585
Belstowe <earl.inis.mona@protonmail.com>
2021-09-17 16:07:48 +0700

    Сделал проверку на аргументы

--- src/calc/main.cpp
+++ src/calc/main.cpp
@@ -8,14 +8,20 @@
 int main(int argc, char **argv) {
 	CLI::App app("BelMoProTech Lab #1");
 
-	double a, b;
+	double a = .0, b = .0;
 	app.add_option<double>("--first", a, "First number");
 	app.add_option<double>("--second", b, "Second number");
-
+	
 	try {
 		app.parse(argc, argv);
 	} catch (const CLI::ParseError &e) {
 		return app.exit(e);
+	}
+
+	if ((app.count("--first") != 1) || (app.count("--second") != 1)) {
+		std::cout << "Valid use is:" << std::endl;
+		std::cout << " $ ./calc_app --first {num} --second {num}" << std::endl;
+		return 0;
 	}
 
 	std::cout << Summator::sum(a, b) << std::endl;

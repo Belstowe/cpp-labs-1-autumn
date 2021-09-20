1647b4aebc3f1f633e8c1460ef66f370079d5252
Belstowe <earl.inis.mona@protonmail.com>
2021-09-20 09:59:41 +0700

    Оформление кода по Clang Format

--- tests/libcalc/sum.test.cpp
+++ tests/libcalc/sum.test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 #include <libcalc/sum.hpp>
 
-TEST(summatorTest, accuracyCheck) {
-	EXPECT_DOUBLE_EQ(Summator::sum(2, 40), 42);
-	EXPECT_DOUBLE_EQ(Summator::sum(1, 2), 3);
-	EXPECT_DOUBLE_EQ(Summator::sum(3.1415926535, 2.7182818284), 5.8598744819);
+TEST(summatorTest, accuracyCheck)
+{
+    EXPECT_DOUBLE_EQ(Summator::sum(2, 40), 42);
+    EXPECT_DOUBLE_EQ(Summator::sum(1, 2), 3);
+    EXPECT_DOUBLE_EQ(Summator::sum(3.1415926535, 2.7182818284), 5.8598744819);
 }

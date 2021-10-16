a3a1f1e0c6028664c7cf447ecfbd6fea6c119d8a
Belstowe <earl.inis.mona@protonmail.com>
2021-10-16 19:13:51 +0700

    Базовая реализация ввода/вывода программы

--- src/sql_parser/main.cpp
+++ src/sql_parser/main.cpp
@@ -3,6 +3,7 @@
 #include "CLI/Formatter.hpp"
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char* argv[])
@@ -12,8 +13,13 @@ int main(int argc, char* argv[])
     std::string input_file;
     std::string output_file;
 
-    app.add_option<std::string>("-i,--input", input_file, "Input File");
-    app.add_option<std::string>("-o,--output", output_file, "Output File");
+    std::ifstream input_file_stream;
+    std::ofstream output_file_stream;
+
+    CLI::Option *opt_i = app.add_option<std::string>("-i,--input", input_file, "Input File");
+    CLI::Option *opt_o = app.add_option<std::string>("-o,--output", output_file, "Output File");
+
+    opt_i->required();
 
     try {
         app.parse(argc, argv);
@@ -21,5 +27,19 @@ int main(int argc, char* argv[])
         return app.exit(e);
     }
 
+    input_file_stream.open(input_file, std::ifstream::in);
+    if (*opt_o)
+        output_file_stream.open(output_file, std::ofstream::out | std::ofstream::app);
+
+    /* 
+     * In this section we would then pass our Input Stream to Lexer;
+     * Lexer would then return sequence of Tokens;
+     * This sequence would then, together with Output Stream, get passed into Parser.
+     */
+
+    input_file_stream.close();
+    if (output_file_stream.is_open())
+        output_file_stream.close();
+    
     return 0;
 }

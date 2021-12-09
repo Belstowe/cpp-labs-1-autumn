39e39f8e57f139bbaa9cd015a9981ff16c02f72b
Belstowe <earl.inis.mona@protonmail.com>
2021-12-10 00:54:46 +0700

    Вывод выражений: табуляция, названия

--- src/librdb/parser/SqlStatement.cpp
+++ src/librdb/parser/SqlStatement.cpp
@@ -38,15 +38,16 @@ CreateTableStatement::CreateTableStatement(std::string table_name, std::vector<C
 }
 
 void CreateTableStatement::print(std::ostream& os) const {
-    os << "{ \n";
-    os << "\"table_name\": " << _table_name << ",\n";
-    os << "\"column_def_seq\": [\n";
+    os << "\"create_statement\":\n\t";
+    os << "{ \n\t";
+    os << "\"table_name\": " << _table_name << ",\n\t";
+    os << "\"column_def_seq\": [\n\t";
     for (auto&& column_def : _column_def_seq) {
         os << "\t{ ";
         os << "\"column_name\": " << column_def.column_name << ", ";
         os << "\"type\": " << column_def.type_name;
         os << " } ";
-        os << "\n";
+        os << "\n\t";
     }
     os << "] }";
 }
@@ -57,15 +58,16 @@ InsertStatement::InsertStatement(std::string table_name, std::vector<std::string
 }
 
 void InsertStatement::print(std::ostream& os) const {
-    os << "{ \n";
-    os << "\"table_name\": " << _table_name << ",\n";
-    os << "\"column_write_seq\": [\n";
+    os << "\"insert_statement\":\n\t";
+    os << "{ \n\t";
+    os << "\"table_name\": " << _table_name << ",\n\t";
+    os << "\"column_write_seq\": [\n\t";
     for (size_t index = 0; index < _column_name_seq.size(); index++) {
         os << "\t{ ";
         os << "\"column_name\": " << _column_name_seq.at(index) << ", ";
         os << "\"value\": " << _value_seq.at(index);
         os << " } ";
-        os << "\n";
+        os << "\n\t";
     }
     os << "] }";
 }
@@ -77,19 +79,20 @@ SelectStatement::SelectStatement(std::string table_name, std::vector<std::string
 }
 
 void SelectStatement::print(std::ostream& os) const {
-    os << "{ \n";
-    os << "\"table_name\": " << _table_name << ",\n";
-    os << "\"column_name_seq\": [\n";
+    os << "\"select_statement\":\n\t";
+    os << "{ \n\t";
+    os << "\"table_name\": " << _table_name << ",\n\t";
+    os << "\"column_name_seq\": [\n\t";
     for (auto&& column_name : _column_name_seq) {
         os << "\t{ ";
         os << "\"column_name\": " << column_name;
         os << " } ";
-        os << "\n";
+        os << "\n\t";
     }
     os << "] ";
     if (_has_expression_cond) {
-        os << ",\n";
-        os << "\"expression\": " << _expression << "\n";
+        os << ",\n\t";
+        os << "\"expression\": " << _expression << "\n\t";
     }
     os << "}";
 }
@@ -101,13 +104,14 @@ DeleteFromStatement::DeleteFromStatement(std::string table_name, Expression expr
 }
 
 void DeleteFromStatement::print(std::ostream& os) const {
-    os << "{ \n";
+    os << "\"delete_statement\":\n\t";
+    os << "{ \n\t";
     os << "\"table_name\": " << _table_name;
     if (_has_expression_cond) {
-        os << " ,\n";
+        os << " ,\n\t";
         os << "\"expression\": " << _expression;
     }
-    os << "\n}";
+    os << "\n\t}";
 }
 
 DropTableStatement::DropTableStatement(std::string table_name)
@@ -116,7 +120,8 @@ DropTableStatement::DropTableStatement(std::string table_name)
 }
 
 void DropTableStatement::print(std::ostream& os) const {
-    os << "{ \n";
-    os << "\"table_name\": " << _table_name << "\n";
+    os << "\"drop_statement\":\n\t";
+    os << "{ \n\t";
+    os << "\"table_name\": " << _table_name << "\n\t";
     os << "}";
 }
